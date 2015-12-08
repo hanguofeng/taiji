2eb387e678e9100fffef2ca027c33e092d7b43c9
xq262144 <xq262144@gmail.com>
2015-12-08 16:20:22 +0800

    Modify arbiter interface, add error result

--- arbiter.go
+++ arbiter.go
@@ -5,20 +5,12 @@ import "github.com/Shopify/sarama"
 type Arbiter interface {
 	OffsetChannel() chan<- int64
 	MessageChannel() <-chan *sarama.ConsumerMessage
-	Init(config *CallbackItemConfig, arbiterConfig *MapConfig, manager *PartitionManager)
+	Init(config *CallbackItemConfig, arbiterConfig ArbiterConfig, manager *PartitionManager) error
 	Run()
 	Close()
 }
 
-type SequentialArbiter struct {
-}
-
 // 以下为本次开发的可选内容
-type SlidingWindowArbiter struct {
-}
 
 type LeakyBucketArbiter struct {
-}
-
-type UnboundArbiter struct {
 }
