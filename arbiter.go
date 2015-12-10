1c572f19efb3ea4d81dc4366a7ab56f4bc4a408a
xq262144 <xq262144@gmail.com>
2015-12-10 22:59:59 +0800

    Merge StartStopControl interface into arbiter.go

--- arbiter.go
+++ arbiter.go
@@ -1,16 +1,36 @@
 package main
 
-import "github.com/Shopify/sarama"
+import (
+	"errors"
+	"fmt"
+	"strings"
+
+	"github.com/Shopify/sarama"
+)
 
 type Arbiter interface {
 	OffsetChannel() chan<- int64
 	MessageChannel() <-chan *sarama.ConsumerMessage
 	Init(config *CallbackItemConfig, arbiterConfig ArbiterConfig, manager *PartitionManager) error
-	Run()
-	Close()
+	Run() error
+	Close() error
+	Ready() error
+	WaitForExitChannel() <-chan struct{}
 }
 
-// 以下为本次开发的可选内容
+type ArbiterCreator func() Arbiter
 
-type LeakyBucketArbiter struct {
+var registeredArbiterMap = make(map[string]ArbiterCreator)
+
+func RegisterArbiter(name string, factory ArbiterCreator) {
+	registeredArbiterMap[strings.ToLower(name)] = factory
+}
+
+func NewArbiter(name string) (Arbiter, error) {
+	name = strings.ToLower(name)
+	if registeredArbiterMap[name] == nil {
+		return nil, errors.New(fmt.Sprintf("Arbiter %s not exists", name))
+	}
+
+	return registeredArbiterMap[name](), nil
 }
