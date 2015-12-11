a42c146623a3532d6db040ba12c2ed0e8a943ac2
xq262144 <xq262144@gmail.com>
2015-12-11 12:05:02 +0800

    Add Ready/markReady feature to StartStopControl

--- mixin.go
+++ mixin.go
@@ -6,6 +6,7 @@ import (
 )
 
 type StartStopControl struct {
+	ready   chan struct{}
 	stopper chan struct{}
 	stopped chan struct{}
 }
@@ -21,7 +22,6 @@ func (ssc *StartStopControl) ensureStart() error {
 }
 
 func (ssc *StartStopControl) markStart() {
-
 	ssc.stopper = make(chan struct{})
 	ssc.stopped = make(chan struct{})
 }
@@ -49,32 +49,38 @@ func (ssc *StartStopControl) markClosing() {
 }
 
 func (ssc *StartStopControl) WaitForClose() {
-	if ssc.stopper != nil {
-		select {
-		case <-ssc.stopper:
-		}
+	select {
+	case <-ssc.WaitForCloseChannel():
 	}
 }
 
 func (ssc *StartStopControl) WaitForExit() {
-	if ssc.stopped != nil {
-		select {
-		case <-ssc.stopped:
-		}
+	select {
+	case <-ssc.WaitForExitChannel():
 	}
 }
 
 func (ssc *StartStopControl) WaitForCloseChannel() <-chan struct{} {
+	// TODO, use atomic semaphore wait
+	if ssc.stopper != nil {
+		time.Sleep(1 * time.Second)
+	}
 	return ssc.stopper
 }
 
 func (ssc *StartStopControl) WaitForExitChannel() <-chan struct{} {
+	// TODO, use atomic semaphore wait
+	if ssc.stopped != nil {
+		time.Sleep(1 * time.Second)
+	}
 	return ssc.stopped
 }
 
 func (ssc *StartStopControl) Close() error {
 	ssc.AsyncClose()
-	ssc.WaitForExit()
+	if !ssc.Closed() {
+		ssc.WaitForExit()
+	}
 	return nil
 }
 
@@ -115,4 +121,37 @@ func (ssc *StartStopControl) Closing() bool {
 
 func (ssc *StartStopControl) Running() bool {
 	return !ssc.Closed() && !ssc.Closing()
+}
+
+func (ssc *StartStopControl) initReady() {
+	ssc.ready = make(chan struct{})
+}
+
+func (ssc *StartStopControl) markReady() {
+	if ssc.ready != nil {
+		select {
+		case <-ssc.ready:
+		default:
+			close(ssc.ready)
+		}
+	}
+}
+
+func (ssc *StartStopControl) Ready() error {
+	select {
+	case <-ssc.ReadyChannel():
+	case <-ssc.WaitForExitChannel():
+		return errors.New("Service stopped")
+	}
+
+	return nil
+}
+
+func (ssc *StartStopControl) ReadyChannel() <-chan struct{} {
+	// TODO, use atomic semaphore wait
+	for ssc.ready == nil {
+		time.Sleep(1 * time.Second)
+	}
+
+	return ssc.ready
 }
