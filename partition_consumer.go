6d1b9b3d479a25adece74295cae1080c2926c8db
xq262144 <xq262144@gmail.com>
2015-12-11 12:09:54 +0800

    Refactor PartitionManager, divide into PartitionManager/PartitionConsumer

---
+++ partition_consumer.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"time"
+
+	"github.com/Shopify/sarama"
+	"github.com/cihub/seelog"
+	"github.com/wvanbergen/kazoo-go"
+)
+
+const PARTITION_CONSUMER_CHANNEL_BUFFER_SIZE = 256
+
+type PartitionConsumer struct {
+	*StartStopControl
+	Topic     string
+	Partition int32
+	consumer  sarama.PartitionConsumer
+	manager   *PartitionManager
+	config    *CallbackItemConfig
+}
+
+func NewPartitionConsumer() *PartitionConsumer {
+	return &PartitionConsumer{
+		StartStopControl: &StartStopControl{},
+	}
+}
+
+func (pc *PartitionConsumer) Init(config *CallbackItemConfig, topic string, partition int32, manager *PartitionManager) error {
+	pc.config = config
+	pc.Topic = topic
+	pc.Partition = partition
+
+	return nil
+}
+
+func (pc *PartitionConsumer) Run() error {
+	// start stop control
+	if err := pc.ensureStart(); err != nil {
+		return err
+	}
+
+	// defer is executed reversed to declaration order
+	defer pc.markStop()
+	pc.initReady()
+
+	// claim partition, zk /consumers/CG/owners
+	for maxRetries, tries := 5, 0; tries < maxRetries; tries++ {
+		if err := pc.manager.GetCallbackManager().GetKazooGroupInstance().ClaimPartition(pc.Topic, pc.Partition); err == nil {
+			break
+		} else if err == kazoo.ErrPartitionClaimedByOther && tries+1 < maxRetries {
+			time.Sleep(time.Duration(5*tries) * time.Second)
+		} else {
+			seelog.Errorf("Failed to claim the partition [topic:%s][partition:%d][err:%s]", pc.Topic, pc.Partition, err)
+			return err
+		}
+	}
+
+	defer pc.manager.GetCallbackManager().GetKazooGroupInstance().ReleasePartition(pc.Topic, pc.Partition)
+
+	// initialize partition offset manager instance
+	nextOffset, err := pc.manager.GetCallbackManager().GetOffsetManager().InitializePartition(pc.Topic, pc.Partition)
+	if err != nil {
+		seelog.Errorf("Failed to determine initial offset [topic:%s][partition:%d][err:%s]", pc.Topic, pc.Partition, err)
+		return err
+	}
+
+	defer pc.manager.GetCallbackManager().GetOffsetManager().FinalizePartition(pc.Topic, pc.Partition)
+
+	// start partition consumer instance
+	if nextOffset >= 0 {
+		seelog.Infof("Partition consumer starting at offset [topic:%s][partition:%d][nextoffset:%d]", pc.Topic, pc.Partition, nextOffset)
+	} else {
+		if pc.config.InitialFromOldest {
+			seelog.Infof("Partition consumer starting at the oldest available offset [topic:%s][partition:%d]", pc.Topic, pc.Partition)
+			nextOffset = sarama.OffsetOldest
+		} else {
+			seelog.Infof("Partition consumer listening for new messages only [topic:%s][partition:%d]", pc.Topic, pc.Partition)
+			nextOffset = sarama.OffsetNewest
+		}
+	}
+
+	pc.consumer, err = pc.manager.GetCallbackManager().GetConsumer().ConsumePartition(pc.Topic, pc.Partition, nextOffset)
+	if err == sarama.ErrOffsetOutOfRange {
+		seelog.Infof("Partition consumer offset out of range [topic:%s][partition:%d]", pc.Topic, pc.Partition)
+		// if the offset is out of range, simplistically decide whether to use OffsetNewest or OffsetOldest
+		// if the configuration specified offsetOldest, then switch to the oldest available offset, else
+		// switch to the newest available offset.
+		if pc.config.InitialFromOldest {
+			nextOffset = sarama.OffsetOldest
+			seelog.Infof("Partition consumer offset reset to oldest available offset [topic:%s][partition:%d]", pc.Topic, pc.Partition)
+		} else {
+			nextOffset = sarama.OffsetNewest
+			seelog.Infof("Partition consumer offset reset to newest available offset [topic:%s][partition:%d]", pc.Topic, pc.Partition)
+		}
+		// retry the consumePartition with the adjusted offset
+		pc.consumer, err = pc.manager.GetCallbackManager().GetConsumer().ConsumePartition(pc.Topic, pc.Partition, nextOffset)
+	}
+
+	if err != nil {
+		seelog.Errorf("Failed to start partition consumer [topic:%s][partition:%d][err:%s]", pc.Topic, pc.Partition, err)
+		return err
+	}
+
+	defer pc.consumer.Close()
+
+	// start stop control
+	if !pc.Running() {
+		return nil
+	}
+	pc.markReady()
+
+partitionConsumerLoop:
+	for {
+		select {
+		case <-pc.WaitForCloseChannel():
+			break partitionConsumerLoop
+		case err := <-pc.consumer.Errors():
+			seelog.Warnf("Received consumer error message [topic:%s][partition:%d][err:%s]",
+				pc.Topic, pc.Partition, err)
+		}
+	}
+
+	seelog.Infof("Stopping partition consumer at offset [topic:%s][partition:%d]", pc.Topic, pc.Partition)
+
+	return nil
+}
+
+func (pc *PartitionConsumer) GetConsumer() sarama.PartitionConsumer {
+	return pc.consumer
+}
--- partition_manager.go
+++ partition_manager.go
@@ -6,149 +6,119 @@ import (
 )
 
 type PartitionManager struct {
-	Topic       string
-	Partition   int32
-	consumer    sarama.PartitionConsumer
-	arbiter     Arbiter
-	transporter []Transporter
-	manager     *CallbackManager
-	messages    chan *sarama.ConsumerMessage
-	errors      chan *sarama.ConsumerError
+	*StartStopControl
+	Topic     string
+	Partition int32
+
+	// parent
+	manager *CallbackManager
+
+	// config
+	config *CallbackItemConfig
+
+	// partition consumer
+	partitionConsumer *PartitionConsumer
+
+	// arbiter
+	arbiter Arbiter
+
+	// transporter
+	transporter       []Transporter
+	transporterRunner *ServiceRunner
 }
 
-func NewPartitionManager(topic string) *PartitionManager {
+func NewPartitionManager() *PartitionManager {
 	return &PartitionManager{
-		Topic: topic,
+		StartStopControl: &StartStopControl{},
 	}
 }
 
-func (this *PartitionManager) Init(config *CallbackItemConfig, topic string, partition int32, manager *CallbackManager) (err error) {
+func (this *PartitionManager) Init(config *CallbackItemConfig, topic string, partition int32, manager *CallbackManager) error {
 	this.Topic = topic
 	this.Partition = partition
 
-	// init arbiter
-	this.arbiter = nil
-	// init worker
-	this.transporter = nil
+	// parent
+	this.manager = manager
 
-	for maxRetries, tries := 5, 0; tries < maxRetries; tries++ {
-		if err := this.manager.kazooGroupInstance.ClaimPartition(this.Topic, this.Partition); err == nil {
-			break
-		} else if err == kazoo.ErrPartitionClaimedByOther && tries+1 < maxRetries {
-			time.Sleep(time.Duration(5*tries) * time.Second)
+	// config
+	this.config = config
+
+	// init partitionConsumer
+	this.partitionConsumer = NewPartitionConsumer()
+	this.partitionConsumer.Init(config, topic, partition, this)
+
+	// init arbiter
+	var err error
+	if this.arbiter, err = NewArbiter(config.ArbiterName); err != nil {
+		return err
+	}
+	if err = this.arbiter.Init(config, config.ArbiterConfig, this); err != nil {
+		return err
+	}
+
+	// init transporter
+	this.transporterRunner = NewServiceRunner()
+
+	return nil
+}
+
+func (this *PartitionManager) Run() error {
+	if err := this.ensureStart(); err != nil {
+		return err
+	}
+
+	defer this.markStop()
+
+	// start partition consumer
+	go this.partitionConsumer.Run()
+	defer this.partitionConsumer.Close()
+	if err := this.partitionConsumer.Ready(); err != nil {
+		seelog.Errorf("Partition consumer start failed [err:%s]", err)
+		return err
+	}
+
+	// start arbiter
+	go this.arbiter.Run()
+	defer this.arbiter.Close()
+	if err := this.arbiter.Ready(); err != nil {
+		seelog.Errorf("Arbiter start failed [err:%s]", err)
+		return err
+	}
+
+	// start transporter group
+	this.transporter = make([]Transporter, 0)
+	for i := 0; i != this.config.WorkerNum; i++ {
+		if transporter, err := NewTransporter(this.config.TransporterName); err == nil {
+			if err := transporter.Init(this.config, this.config.TransporterConfig, this); err != nil {
+				return err
+			}
+			this.transporter = append(this.transporter, transporter)
 		} else {
-			seelog.Errorf("Failed to claim the partition [topic:%s][partition:%d][err:%s]", this.Topic, this.Partition, err)
 			return err
 		}
 	}
+	this.transporterRunner.RetryTimes = len(this.transporter) * 3
+	this.transporterRunner.RunAsync(this.transporter)
+	defer this.transporterRunner.Close()
 
-	nextOffset, err := this.manager.GetOffsetManager().InitializePartition(this.Topic, this.Partition)
-	if err != nil {
-		seelog.Errorf("Failed to determine initial offset [topic:%s][partition:%d][err:%s]", this.Topic, this.Partition, err)
-		return
-	}
-
-	if nextOffset >= 0 {
-		seelog.Infof("Partition consumer starting at offset [topic:%s][partition:%d][nextoffset:%d]", this.Topic, this.Partition, nextOffset)
-	} else {
-		nextOffset = this.manager.cgConfig.Offsets.Initial
-		if nextOffset == sarama.OffsetOldest {
-			seelog.Infof("Partition consumer starting at the oldest available offset [topic:%s][partition:%d]", this.Topic, this.Partition)
-		} else if nextOffset == sarama.OffsetNewest {
-			seelog.Infof("Partition consumer listening for new messages only [topic:%s][partition:%d]", this.Topic, this.Partition)
-		}
-	}
-
-	this.consumer, err = this.manager.saramaConsumer.ConsumePartition(this.Topic, this.Partition, nextOffset)
-	if err == sarama.ErrOffsetOutOfRange {
-		seelog.Infof("Partition consumer offset out of range [topic:%s][partition:%d]", this.Topic, this.Partition)
-		// if the offset is out of range, simplistically decide whether to use OffsetNewest or OffsetOldest
-		// if the configuration specified offsetOldest, then switch to the oldest available offset, else
-		// switch to the newest available offset.
-		if this.manager.cgConfig.Offsets.Initial == sarama.OffsetOldest {
-			nextOffset = sarama.OffsetOldest
-			seelog.Infof("Partition consumer offset reset to oldest available offset [topic:%s][partition:%d]", this.Topic, this.Partition)
-		} else {
-			nextOffset = sarama.OffsetNewest
-			seelog.Infof("Partition consumer offset reset to newest available offset [topic:%s][partition:%d]", this.Topic, this.Partition)
-		}
-		// retry the consumePartition with the adjusted offset
-		this.consumer, err = this.manager.saramaConsumer.ConsumePartition(this.Topic, this.Partition, nextOffset)
-	}
-	if err != nil {
-		seelog.Errorf("Failed to start partition consumer [topic:%s][partition:%d][err:%s]", this.Topic, this.Partition, err)
-		return
+	select {
+	case <-this.partitionConsumer.WaitForExitChannel():
+	case <-this.arbiter.WaitForExitChannel():
+	case <-this.transporterRunner.WaitForExitChannel():
+	case <-this.WaitForCloseChannel():
 	}
 
 	return nil
-}
-
-func (this *PartitionManager) Run(wg *sync.WaitGroup, stopper <-chan struct{}) {
-	for {
-		defer wg.Done()
-
-		select {
-		case <-stopper:
-			return
-		default:
-		}
-
-		var lastOffset int64 = -1 // aka unknown
-	partitionConsumerLoop:
-		for {
-			select {
-			case <-stopper:
-				break partitionConsumerLoop
-
-			case err := <-this.consumer.Errors():
-				select {
-				case this.errors <- err:
-					continue partitionConsumerLoop
-
-				case <-stopper:
-					break partitionConsumerLoop
-				}
-
-			case message := <-this.consumer.Messages():
-				select {
-				case <-stopper:
-					break partitionConsumerLoop
-
-				case this.messages <- message:
-					lastOffset = message.Offset
-				}
-			}
-		}
-
-		seelog.Infof("Stopping partition consumer at offset [topic:%s][partition:%d][lastoffset:%d]", this.Topic, this.Partition, lastOffset)
-		if err := this.manager.GetOffsetManager().FinalizePartition(this.Topic, this.Partition); err != nil {
-			seelog.Errorf("Finalize parttion consumer [topic:%s][partition:%d][err:%s]", this.Topic, this.Partition, err)
-		}
-	}
-
-	// run consumer
-	// run arbiter
-	// run worker
-}
-
-func (this *PartitionManager) Close() {
-	// close consumer
-	// close worker
-	// close arbiter
-	this.consumer.Close()
-	this.manager.kazooGroupInstance.ReleasePartition(this.Topic, this.Partition)
-
 }
 
 func (this *PartitionManager) GetConsumer() sarama.PartitionConsumer {
-	return this.consumer
+	return this.partitionConsumer.GetConsumer()
 }
 
-func (*PartitionManager) GetArbiter() Arbiter {
-	return nil
+func (this *PartitionManager) GetArbiter() Arbiter {
+	return this.arbiter
 }
 
 func (this *PartitionManager) GetCallbackManager() *CallbackManager {
 	return this.manager
-
 }
