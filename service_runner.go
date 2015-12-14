8ba819a6bfca6aec20750e10ee8794e08ca08728
xq262144 <xq262144@gmail.com>
2015-12-14 10:52:44 +0800

    Refactor service_runner, use protected type assertion instead of panic/recover

--- service_runner.go
+++ service_runner.go
@@ -50,55 +50,40 @@ func (sr *ServiceRunner) RunAsync(rawServices interface{}) (<-chan error, error)
 
 func (sr *ServiceRunner) sanitizeInput(services interface{}) ([]Runnable, error) {
 	// main function, use to handle recover result
-	runnable, err := func() ([]Runnable, error) {
-		// anonymous function 1, use to handle go recover
-		return func() ([]Runnable, error) {
-			// anonymous function 2, use to do input sanitize work
-			defer func() {
-				if r := recover(); r != nil {
-					// panic
-				}
-			}()
 
-			servicesValue := reflect.ValueOf(services)
+	servicesValue := reflect.ValueOf(services)
 
-			switch servicesValue.Kind() {
-			case reflect.Array, reflect.Slice:
-				break
-			default:
-				return nil, errors.New("Invalid input, must be array")
-			}
-
-			servicesCount := servicesValue.Len()
-			runnableServices := make([]Runnable, 0, servicesCount)
-
-			for i := 0; i != servicesCount; i++ {
-				if !servicesValue.Index(i).CanInterface() {
-					return nil, errors.New("Invalid input, array item should be interface capable")
-				}
-
-				runnableServices = append(runnableServices, servicesValue.Index(i).Interface().(Runnable))
-			}
-
-			return runnableServices, nil
-		}()
-	}()
-
-	if runnable == nil && err == nil {
-		// panic
-		return nil, errors.New("Invalid input, array item must support Run/Close method")
-
-	} else {
-		return runnable, err
+	switch servicesValue.Kind() {
+	case reflect.Array, reflect.Slice:
+		break
+	default:
+		return nil, errors.New("Invalid input, must be slice or array of Runnable")
 	}
+
+	servicesCount := servicesValue.Len()
+	runnableServices := make([]Runnable, 0, servicesCount)
+
+	for i := 0; i != servicesCount; i++ {
+		if !servicesValue.Index(i).CanInterface() {
+			return nil, errors.New("Invalid input, slice item should be Runnable")
+		}
+
+		runnable, ok := servicesValue.Index(i).Interface().(Runnable)
+
+		if !ok {
+			return nil, errors.New("Invalid input, slice item should be Runnable")
+		}
+
+		runnableServices = append(runnableServices, runnable)
+	}
+
+	return runnableServices, nil
 }
 
 func (sr *ServiceRunner) run(services []Runnable, daemon bool) (<-chan error, error) {
-	if sr.Running() {
-		return nil, errors.New("Service is running")
+	if err := sr.ensureStart(); err != nil {
+		return nil, err
 	}
-
-	sr.markStart()
 
 	event := make(chan int)
 	errorEvent := make(chan error, sr.RetryTimes+1)
