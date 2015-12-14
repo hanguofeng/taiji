d7d8bb5bb5a8a4ab0ffae5b2f3caddc731d0363a
xq262144 <xq262144@gmail.com>
2015-12-14 18:18:36 +0800

    Add NullTransporter for stress test purpose

---
+++ null_transporter.go
@@ -0,0 +1,44 @@
+package main
+
+import "github.com/cihub/seelog"
+
+type NullTransporter struct {
+	config            *CallbackItemConfig
+	transporterConfig TransporterConfig
+	manager           *PartitionManager
+}
+
+func NewNullTransporter() Transporter {
+	return &NullTransporter{}
+}
+
+func (nt *NullTransporter) Init(config *CallbackItemConfig, transporterConfig TransporterConfig, manager *PartitionManager) error {
+	nt.config = config
+	nt.transporterConfig = transporterConfig
+	nt.manager = manager
+
+	return nil
+}
+
+func (nt *NullTransporter) Run() error {
+	arbiter := nt.manager.GetArbiter()
+	messages := arbiter.MessageChannel()
+	offsets := arbiter.OffsetChannel()
+
+	for message := range messages {
+		seelog.Infof("Committed message [topic:%s][partition:%d][url:%s][offset:%d]",
+			message.Topic, message.Partition, nt.config.Url, message.Offset)
+		offsets <- message.Offset
+	}
+
+	return nil
+}
+
+func (nt *NullTransporter) Close() error {
+	// dummy
+	return nil
+}
+
+func init() {
+	RegisterTransporter("Null", NewNullTransporter)
+}
