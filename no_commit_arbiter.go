8bb7d8ecfbf7226040106977ce7bd9bd14756b2e
xq262144 <xq262144@gmail.com>
2015-12-15 12:22:16 +0800

    Add no_commit_arbiter for stress test use

---
+++ no_commit_arbiter.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"github.com/Shopify/sarama"
+	"github.com/cihub/seelog"
+)
+
+type NoCommitArbiter struct {
+	*StartStopControl
+
+	// input/output
+	offsets  chan int64
+	messages chan *sarama.ConsumerMessage
+
+	// parent
+	manager *PartitionManager
+
+	// config
+	config        *CallbackItemConfig
+	arbiterConfig ArbiterConfig
+}
+
+func NewNoCommitArbiter() Arbiter {
+	return &NoCommitArbiter{
+		StartStopControl: &StartStopControl{},
+	}
+}
+
+func (sa *NoCommitArbiter) OffsetChannel() chan<- int64 {
+	return sa.offsets
+}
+
+func (sa *NoCommitArbiter) MessageChannel() <-chan *sarama.ConsumerMessage {
+	return sa.manager.GetConsumer().Messages()
+}
+
+func (sa *NoCommitArbiter) Init(config *CallbackItemConfig, arbiterConfig ArbiterConfig, manager *PartitionManager) error {
+	sa.manager = manager
+	sa.config = config
+	sa.arbiterConfig = arbiterConfig
+
+	return nil
+}
+
+func (sa *NoCommitArbiter) Run() error {
+	if err := sa.ensureStart(); err != nil {
+		return err
+	}
+	defer sa.markStop()
+
+	sa.offsets = make(chan int64, 256)
+	sa.markReady()
+
+arbiterLoop:
+	for {
+		select {
+		case <-sa.WaitForCloseChannel():
+			seelog.Debugf("Stop event triggered [url:%s]", sa.config.Url)
+			break arbiterLoop
+		case offset := <-sa.offsets:
+			seelog.Debugf("Read offset from Transporter [topic:%s][partition:%d][url:%s][offset:%d]",
+				sa.manager.Topic, sa.manager.Partition, sa.config.Url, offset)
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	RegisterArbiter("NoCommit", NewNoCommitArbiter)
+}
