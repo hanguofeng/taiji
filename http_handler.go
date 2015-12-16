1b0657fe7c29911439bfecd28a3aa998bfb3abf4
xq262144 <xq262144@gmail.com>
2015-12-16 17:03:10 +0800

    Add jsonify helper to http_handler.go

--- http_handler.go
+++ http_handler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+
+	"github.com/golang/glog"
 )
 
 // Http Handler
@@ -24,4 +27,41 @@ func (h *HttpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(404)
 	io.WriteString(w, "Path not exists: "+r.URL.Path)
+}
+
+func jsonify(w http.ResponseWriter, r *http.Request, data interface{}, code int) {
+	res := map[string]interface{}{
+		"errno":  code,
+		"errmsg": "success",
+		"data":   data,
+	}
+
+	prettyPrint := false
+
+	r.ParseForm()
+
+	if r.Form["pretty"] != nil && len(r.Form["pretty"]) > 0 {
+		prettyPrint = true
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(200)
+
+	if prettyPrint {
+		prefix := ""
+		indent := "    "
+		if buffer, err := json.MarshalIndent(res, prefix, indent); err != nil {
+			glog.Errorf("Marshal http response error [err:%s]", err)
+		} else {
+			w.Write(buffer)
+		}
+	} else {
+		if buffer, err := json.Marshal(res); err != nil {
+			glog.Errorf("Marshal http response error [err:%s]", err)
+		} else {
+			w.Write(buffer)
+		}
+	}
+
+	return
 }
