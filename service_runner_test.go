f630866b4c5ad9ab893b0436ee0044ee77ca2d93
xq262144 <xq262144@gmail.com>
2016-01-04 15:50:49 +0800

    Test case for not starting Service when runner is closing

--- service_runner_test.go
+++ service_runner_test.go
@@ -43,7 +43,7 @@ func TestServiceRunner(t *testing.T) {
 	runner.RetryTimes = 100
 	service = &TestRunnableService{}
 	runner.Run([]*TestRunnableService{service})
-	assert.Equal(t, uint64(101), service.StartTimes, "Service does not start 101 times")
+	assert.True(t, 101 >= service.StartTimes, "Service does not start not over 101 times")
 	assert.Equal(t, uint64(1), service.CloseTimes, "Service does not close 1 times")
 
 	// test multiple services
@@ -59,12 +59,17 @@ func TestServiceRunner(t *testing.T) {
 	for i := 0; i != 10; i++ {
 		totalRetries += services[i].StartTimes
 		totalCloseTimes += services[i].CloseTimes
+		if services[i].StartTimes <= 0 {
+			// not started ever
+			t.Fatal("Some service is never started")
+		}
 	}
-	assert.Equal(t, uint64(100+10), totalRetries, "Service does not start 100 + 10 times")
+	assert.True(t, (100+10) >= totalRetries, "Service does not start not over 100 + 10 times")
 	assert.Equal(t, uint64(10), totalCloseTimes, "Service does not close 10 times")
 
 	// test error channel
 	errorChannel, err := runner.Run(services)
+	time.Sleep(time.Second)
 	assert.Nil(t, err, "Runner should run successfully")
 	assert.Equal(t, 100+10, len(errorChannel), "Runner must contain 100 + 10 errors in error channel")
 
@@ -73,5 +78,6 @@ func TestServiceRunner(t *testing.T) {
 	assert.Nil(t, err, "Runner should run successfully")
 	// wait for exit
 	runner.WaitForExit()
+	time.Sleep(time.Second)
 	assert.Equal(t, 100+10, len(errorChannel), "Runner must contain 100 + 10 errors in error channel")
 }
